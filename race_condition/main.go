f72602b326cd6d1dee2c24d323984d413ed52ad4
iamdevtry <tranbinhphongcmg@gmail.com>
2023-05-20 14:59:16 +0700

    unit test main file

--- race_condition/main.go
+++ race_condition/main.go
@@ -1,23 +1,47 @@
 package main
 
-// import "sync"
+import "sync"
 
-// var wg sync.WaitGroup
-// var msg string
+var wg sync.WaitGroup
+var msg string
 
-// func updateMessage(s string) {
+func updateMessage(s string) {
+	defer wg.Done()
+
+	msg = s
+}
+
+func main() {
+	msg = "Xin chao!"
+	//solve race condition problem
+	//solve race condition problem
+	wg.Add(3)
+	go updateMessage("Hello!")
+	go updateMessage("Hola!")
+	go updateMessage("Ni hao!")
+	wg.Wait()
+
+	println(msg)
+
+}
+
+// func updateMessage(s string, m *sync.Mutex) {
 // 	defer wg.Done()
+
+// 	m.Lock()
 // 	msg = s
+// 	m.Unlock()
 // }
 
 // func main() {
 // 	msg = "Xin chao!"
+// 	var mutex sync.Mutex
 // 	//solve race condition problem
 // 	//solve race condition problem
 // 	wg.Add(3)
-// 	go updateMessage("Hello!")
-// 	go updateMessage("Hola!")
-// 	go updateMessage("Ni hao!")
+// 	go updateMessage("Hello!", &mutex)
+// 	go updateMessage("Hola!", &mutex)
+// 	go updateMessage("Ni hao!", &mutex)
 // 	wg.Wait()
 
 // 	println(msg)
